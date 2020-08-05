caea100b3d5ce84bd01e1890a466f93caca20d92
Ronak Katta <ronakkatta28@gmail.com>
2020-08-05 23:00:48 +0530

    deploy ready

---
+++ build/precache-manifest.a2797084d908813ed626ab76b81b7f85.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "589ad9c831fb4208e6c8",
+    "url": "/static/css/main.f6389990.chunk.css"
+  },
+  {
+    "revision": "589ad9c831fb4208e6c8",
+    "url": "/static/js/main.589ad9c8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "fd7a499bfe676e3d760d",
+    "url": "/static/js/2.fd7a499b.chunk.js"
+  },
+  {
+    "revision": "bf56edc6c7090e92662e37a9b9ed2aea",
+    "url": "/static/media/project1.bf56edc6.png"
+  },
+  {
+    "revision": "f7e79c7d61c8e4a45847337bcec527e6",
+    "url": "/static/media/project2.f7e79c7d.png"
+  },
+  {
+    "revision": "7bded960f9e6263b998eb4529af6e041",
+    "url": "/static/media/project3.7bded960.png"
+  },
+  {
+    "revision": "581f21772fa25a1a162c07215d469338",
+    "url": "/static/media/project4.581f2177.png"
+  },
+  {
+    "revision": "3cd5b298c087157c71edfbebf64c1f3e",
+    "url": "/static/media/project5.3cd5b298.png"
+  },
+  {
+    "revision": "fd99f8e3744a0cf1335282d72cb77f5f",
+    "url": "/static/media/email_icon.fd99f8e3.png"
+  },
+  {
+    "revision": "e2a6288611c5c203fdca4761ca408acf",
+    "url": "/static/media/github_icon.e2a62886.png"
+  },
+  {
+    "revision": "56ab00e848077ea3fec713da56e69681",
+    "url": "/static/media/linkedin_icon.56ab00e8.png"
+  },
+  {
+    "revision": "9e286e8bf57e945dbbaadff88dcd1ba5",
+    "url": "/static/media/profile.9e286e8b.png"
+  },
+  {
+    "revision": "17589978b091a37d53788db767208dc9",
+    "url": "/index.html"
+  }
+];
