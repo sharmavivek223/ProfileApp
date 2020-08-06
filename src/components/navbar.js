e7c101d5c617bdf0ee6c334b23522907230cf4a9
Ronak <ronakkatta28@gmail.com>
2020-08-06 14:45:57 +0530

    update

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -49,7 +49,7 @@ function Navbar(){
 	return(
 		<div >
 			<nav className="navbar fixed-top navbar-expand-lg navbar-light">
-  <a class="navbar-brand" href="/">Profile</a>
+  <a class="navbar-brand" href="/">Ronak Katta</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
